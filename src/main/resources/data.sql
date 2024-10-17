9968b5b3e1409ebc62d28ad753ffba6da3470d57
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-10-17 15:47:32 +0330

    cleanup + setup JdbcTemplate using JdbcTemplate

--- src/main/resources/data.sql
+++
@@ -1,6 +0,0 @@
-INSERT INTO widgets (id, name, purpose) VALUES
-    (1, 'Widget A', 'Used for testing purpose.'),
-    (2, 'Widget B', 'Designd for entertainment.'),
-    (3, 'Widget C', 'Enhances productivity.'),
-    (4, 'Widget D', 'Perfect for outdoor activity.'),
-    (5, 'Widget E', 'Improves overall well-being.');
