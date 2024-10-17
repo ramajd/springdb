9574ea2d3e825929595318be0d3faf53c8918cd4
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-10-17 14:56:50 +0330

    connecting to in-memory H2 database

--- src/main/java/io/github/ramajd/springdb/SpringdbApplication.java
+++ src/main/java/io/github/ramajd/springdb/SpringdbApplication.java
@@ -1,13 +1,30 @@
 package io.github.ramajd.springdb;
 
+import javax.sql.DataSource;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import lombok.extern.java.Log;
 
 @SpringBootApplication
-public class SpringdbApplication {
+@Log
+public class SpringdbApplication implements CommandLineRunner {
+
+	private final DataSource dataSource;
+
+	public SpringdbApplication(final DataSource dataSource) { this.dataSource = dataSource; }
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringdbApplication.class, args);
 	}
 
+	@Override
+	public void run(final String... args) {
+		log.info("dataSource: " + dataSource.toString());
+		final JdbcTemplate resTemplate = new JdbcTemplate(dataSource);
+		resTemplate.execute("select 1");
+	}
 }
