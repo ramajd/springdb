07c4aa9ee10a879b719ef093cf3d6f722b1ab90f
Reza Alizadeh Majd <r.a.majd@gmail.com>
2024-10-17 14:04:27 +0330

    initial commit

---
+++ src/main/java/io/github/ramajd/springdb/SpringdbApplication.java
@@ -0,0 +1,13 @@
+package io.github.ramajd.springdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringdbApplication.class, args);
+	}
+
+}
